1364eeb7303262125a3e87b9fa0d3ec00be16bc1
Nikolaj Reibke <nire@tv2.dk>
2014-02-12 17:37:51 +0100

    small testing in main of searching

--- src/algorithms/MySearchingAlgorithms.java
+++ src/algorithms/MySearchingAlgorithms.java
@@ -4,36 +4,49 @@ public class MySearchingAlgorithms {
 	
 	
 	public static void main(String[] args) {
-		Integer[] arr = new Integer[]{123,6,234,634,5,431,23,723,62,234,2};
-		arr = (Integer[]) MySortingAlgorithms.insertionSort(arr);
+		Integer[] arr = new Integer[0];
+		try {
+			arr = (Integer[]) ArrayGenerator.makeSortedArray(5000, true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+//		arr = (Integer[]) MySortingAlgorithms.insertionSort(arr);
 		System.out.println(MySortingAlgorithms.makeString(arr));
-		System.out.println("Index of value: " + binarySearch(arr, 723));
+		System.out.println("Index of value: " + binarySearch(arr, 17));
 	}
 	
 	public static Integer binarySearch(Integer[] array, Integer v){
 		
 		int modifier = array.length / 2;
 		int index = modifier + 1;
-		System.out.println("Index: " + index);
 		if(array[index] == v){
 			return index;
 		}
+		byte movedByZero = 0;
 		while(!array[index].equals(v)){
 			Integer focus = array[index];
-			System.out.println("value:" + focus + " ? " + v);
-			System.out.println((focus.equals(v)));
 			modifier /= 2;
 			if(focus.equals(v)){
 				System.out.println("was equal");
-				return index;
+				return index ;
 			}else if(focus.compareTo(v) < 0){
-				System.out.println("counting Up by " + modifier);
-				index += modifier < 1 ? 1 : modifier;
-				System.out.println("new Index: " + index);
+				if(modifier < 1){
+					if(movedByZero > 2){
+						return null;
+					}
+					movedByZero++;
+					index += 1;
+				}
+				index += modifier;
 			}else if(focus.compareTo(v) > 0){
-				System.out.println("counting Down by " + modifier);
-				index -= modifier < 1 ? 1 : modifier;
-				System.out.println("new Index: " + index);
+				if(modifier < 1){
+					if(movedByZero > 2){
+						return null;
+					}
+					movedByZero++;
+					index -= 1;
+				}
+				index -= modifier;
 			}
 			if(index < 0 || index > array.length -1){
 				return null;
