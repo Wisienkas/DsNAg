ee398fb2a2f42c4f2dd7d539e488a5c900c21949
Nikolaj Reibke <nire@tv2.dk>
2014-02-17 09:07:07 +0100

    fixed issue with continuous off by ones due to casting ints of uneven numbers divided by 2

--- src/algorithms/MySearchingAlgorithms.java
+++ src/algorithms/MySearchingAlgorithms.java
@@ -6,13 +6,22 @@ public class MySearchingAlgorithms {
 	public static void main(String[] args) {
 		Integer[] arr = new Integer[0];
 		try {
-			arr = (Integer[]) ArrayGenerator.makeSortedArray(5000, true);
+//			arr = (Integer[]) ArrayGenerator.makeSortedArray(5000, true);
+			arr = (Integer[]) ArrayGenerator.makeSortedArray(45, true);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		Integer[] values = new Integer[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			values[i] = (Integer) binarySearch(arr, arr[i]);
+			System.out.println("Value: " + arr[i] + "\tIndex: " + i + "\tStatus: " + values[i]);
+		}
+		System.out.println("Value: " + 99 + "\tIndex: NaN" + "\tStatus: " + binarySearch(arr, 99));
+		System.out.println("Value: " + 56 + "\tIndex: NaN" + "\tStatus: " + binarySearch(arr, 56));
+		System.out.println("Value: " + 87 + "\tIndex: NaN" + "\tStatus: " + binarySearch(arr, 87));
 //		arr = (Integer[]) MySortingAlgorithms.insertionSort(arr);
-		System.out.println(MySortingAlgorithms.makeString(arr));
-		System.out.println("Index of value: " + binarySearch(arr, 431));
+//		System.out.println(MySortingAlgorithms.makeString(arr));
+//		System.out.println("Index of value: " + binarySearch(arr, 431));
 	}
 	
 	/**
@@ -23,8 +32,9 @@ public class MySearchingAlgorithms {
 	 */
 	public static Number binarySearch(Number[] array, Number v){
 		
-		int modifier = array.length / 2;
-		int index = modifier + 1;
+		float modifier = array.length / 2;
+		float finerIndex = modifier;
+		int index = (int) modifier;
 		if(array[index].longValue() == v.longValue()){
 			return index;
 		}
@@ -38,13 +48,17 @@ public class MySearchingAlgorithms {
 			if(modifier < 1){
 				overcount--;
 			}
-			if(focus.equals(v)){
-
+			if(focus.equals(v.longValue())){
 				return index;
 			}else if(focus.compareTo(v.longValue()) < 0){
+				finerIndex += modifier;
 				index += modifier < 1 ? 1 : modifier;
 			}else if(focus.compareTo(v.longValue()) > 0){
+				finerIndex -= modifier;
 				index -= modifier < 1 ? 1 : modifier;
+			}
+			if(index - finerIndex > 1 || finerIndex - index > 1){
+				index = Math.round(finerIndex);
 			}
 			if(index < 0 || index > array.length -1){
 				return null;
