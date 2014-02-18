20715109199caa04b318603eb15f1ef0678cb4e8
Nikolaj Reibke <nikolajsr12@gmail.com>
2014-02-18 01:38:32 +0100

    Added inversion Counter Merge Sort, which count inversions along with merge sorting

--- src/algorithms/InversionCounter.java
+++ src/algorithms/InversionCounter.java
@@ -3,9 +3,14 @@ package algorithms;
 public class InversionCounter {
 	
 	public static void main(String[] args) {
-		int[] array = new int[]{2,3,8,6,1};
-		int count = countInversions(array);
-		int count2 = countInversions2(array);
+		Number[] A = new Number[0];
+		try {
+			A = ArrayGenerator.makeRandomArray(10, 0, 100);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		int count = countInversions(A);
+		int count2 = countInversions2(A, 0 , A.length - 1);
 		System.out.println("Pairs: " + count);
 		System.out.println("Pairs: " + count2);
 	}
@@ -16,33 +21,73 @@ public class InversionCounter {
 	 * @param A to be counted
 	 * @return counts
 	 */
-	public static int countInversions(int[] A){
+	public static int countInversions(Number[] A){
 		int count = 0;
 		
 		for(int i = 0; i < A.length; i++){
 			for (int j = i + 1; j < A.length; j++) {
-				count += (i < j && A[i] > A[j]) ? 1 : 0;
+				count += (i < j && A[i].intValue() > A[j].intValue()) ? 1 : 0;
 			}
 		}
 		return count;
 	}
 	
-	public static int countInversions2(int[] A){
-		if(A.length <= 1){
-			return 0;
+	public static int countInversions2(Number[] A, int start, int end){
+		int result= 0;
+		if(end - start < 1){
+			return result;
 		}
-		int count = 0;
-		//converting to Number[] due to generics
-		Number[] M = new Number[A.length];
-		for(int i = 0; i < A.length; i++){
-			M[i] = (Integer) A[i];
-		}
-		Number[] B = MySortingAlgorithms.mergeSort(M);
-		for(int j = 0; j < B.length; j++){
-			Integer B_value = (Integer) MySearchingAlgorithms.binarySearch(B, M[j]);
-			
-			count += j > B_value ? j - B_value : 0;
-		}
-		return count;
+		int split = ((end - start) / 2) + start;
+		result += countInversions2(A, start, split);
+		result += countInversions2(A, split + 1, end);
+		result += mergeInversion(A, start, end, split);
+		
+		return result;
 	}
+	private static int mergeInversion(Number[] A, int start, int end, int split) {
+		int result = 0;
+		Number[] L = new Number[(split + 1) - start];
+		Number[] R = new Number[end - split];
+		
+		for(int i = 0; i < L.length; i++){
+			L[i] = A[start + i];
+		}
+		for(int i = 0; i < R.length; i++){
+			R[i] = A[split + 1 + i];
+		}
+		int ri = 0;
+		int li = 0;
+		for(int i = start; i <= end; i++){
+			if(L[li].floatValue() <= R[ri].floatValue()){
+				A[i] = L[li];
+				li++;
+			}else{
+				A[i] = R[ri];
+				int LSkip = L.length - li;
+				result +=  LSkip;
+				ri++;
+			}
+			if(li == L.length){
+				while(ri < R.length){
+					i++;
+					A[i] = R[ri];
+					int LSkip = L.length - li;
+					result +=  LSkip;
+					ri++;
+				}
+				return result;
+			}
+			if(ri == R.length){
+				while(li < L.length){
+					i++;
+					A[i] = L[li];
+					li++;
+				}
+				return result;
+			}
+		}
+		return result;
+	}
+	
+	
 }
