c0d6211bb84f7b645774e77491661312dc04a3ce
Nikolaj Reibke <nikolajsr12@gmail.com>
2014-03-16 23:01:19 +0100

    formatting output from test

--- src/algorithms/test/Test.java
+++ src/algorithms/test/Test.java
@@ -9,10 +9,16 @@ import algorithms.quicksort.QuickSort;
 public class Test {
 
 	public static void main(String[] args) {
-		int size = 10000000;
+		int size = 1000000;
 		int k = size * 20;
 		int runs = 5;
 		int sorts = 4;
+		int active;
+		String[] names = new String[]{
+				"Quicksort - Sort 1",
+				"Quicksort - Sort 2",
+				"Countsort - Sort  ",
+				"Mergesort - Sort  "};
 		long[] times = new long[sorts];
 		boolean[][] passed = new boolean[sorts][runs];
 		for (int i = 0; i < runs; i++) {
@@ -24,52 +30,54 @@ public class Test {
 				int[] D = ArrayGenerator.makeRandomArrayPrimi(size, 0, k);
 				
 				
-				System.out.println("Quicksort - Sort 1");
+				active = 0;
+				System.out.print("| " + names[active] + " |\t  Time: ");
 				long time = System.nanoTime();
 				QuickSort.Sort(A, 0, A.length - 1);
-				time = System.nanoTime() - time;
-				time /= 1000000;
-				times[0] += time;
-				passed[0][i] = ArrayUtils.verifyArraySorted(A);
-				System.out.println("time: " + time + "ms");
+				time = (System.nanoTime() - time) / 1000000;
+				times[active] += time;
+				passed[active][i] = ArrayUtils.verifyArraySorted(A);
+				System.out.println(time + "ms");
 				
 				
-				System.out.println("Quicksort - Sort 2");
+				active = 1;
+				System.out.print("| " + names[active] + " |\t  Time: ");
 				time = System.nanoTime();
 				QuickSort.Sort(B, 0, B.length - 1);
-				time = System.nanoTime() - time;
-				time /= 1000000;
-				times[1] += time;
-				passed[1][i] = ArrayUtils.verifyArraySorted(B);
-				System.out.println("time: " + time + "ms");
+				time = (System.nanoTime() - time) / 1000000;
+				times[active] += time;
+				passed[active][i] = ArrayUtils.verifyArraySorted(B);
+				System.out.println(time + "ms");
 				
 				
-				System.out.println("Countsort - Sort");
+				active = 2;
+				System.out.print("| " + names[active] + " |\t  Time: ");
 				time = System.nanoTime();
 				C = CountSort.Sort(C, 0, C.length, k);
-				time = System.nanoTime() - time;
-				time /= 1000000;
-				times[2] += time;
-				passed[2][i] = ArrayUtils.verifyArraySorted(C);
-				System.out.println("time: " + time + "ms");
+				time = (System.nanoTime() - time) / 1000000;
+				times[active] += time;
+				passed[active][i] = ArrayUtils.verifyArraySorted(C);
+				System.out.println(time + "ms");
 				
 				
-				System.out.println("Mergesort - Sort");
+				active = 3;
+				System.out.print("| " + names[active] + " |\t  Time: ");
 				time = System.nanoTime();
 				Mergesort.mergeSort(D, 0, D.length - 1);
-				time = System.nanoTime() - time;
-				time /= 1000000;
-				times[3] += time;
-				passed[3][i] = ArrayUtils.verifyArraySorted(D);
-				System.out.println("time: " + time + "ms");
+				time = (System.nanoTime() - time) / 1000000;
+				times[active] += time;
+				passed[active][i] = ArrayUtils.verifyArraySorted(D);
+				System.out.println(time + "ms");
+				
+				
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 			
 		}
-		System.out.println("Average");
+		System.out.println("\nFinal Results!! \n");
 		for (int j = 0; j < sorts; j++) {
-			System.out.print("total for " + j + ": " + (times[j] / runs) + "ms\t");
+			System.out.print("Average for " + j + ": " + (times[j] / runs) + "ms\t");
 			for (int j2 = 0; j2 < runs; j2++) {
 				System.out.print("status: " + j2 + " == " + passed[j][j2] +" :: ");
 			}
