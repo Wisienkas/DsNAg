ede62b7d44d8a71a370d9586f54f60d45a3e607d
Nikolaj Reibke <nikolajsr12@gmail.com>
2014-03-17 01:58:03 +0100

    added digits to binary and started radix

--- src/algorithms/ArrayUtils.java
+++ src/algorithms/ArrayUtils.java
@@ -19,4 +19,44 @@ public class ArrayUtils {
 		}
 		return true;
 	}
+	
+	public static double[] evolveArray(double[] A){
+		double[] new_A = new double[A.length * 2];
+		for(int i = 0; i < A.length; i++){
+			new_A[i] = A[i];
+		}
+		return new_A;
+	}
+	
+	public static boolean[] evolveArray(boolean[] A){
+		boolean[] new_A = new boolean[A.length * 2];
+		for(int i = 0; i < A.length; i++){
+			new_A[i] = A[i];
+		}
+		return new_A;
+	}
+	
+	public static float[] evolveArray(float[] A){
+		float[] new_A = new float[A.length * 2];
+		for(int i = 0; i < A.length; i++){
+			new_A[i] = A[i];
+		}
+		return new_A;
+	}
+	
+	public static int[] evolveArray(int[] A){
+		int[] new_A = new int[A.length * 2];
+		for(int i = 0; i < A.length; i++){
+			new_A[i] = A[i];
+		}
+		return new_A;
+	}
+	
+	public static long[] evolveArray(long[] A){
+		long[] new_A = new long[A.length * 2];
+		for(int i = 0; i < A.length; i++){
+			new_A[i] = A[i];
+		}
+		return new_A;
+	}
 }
